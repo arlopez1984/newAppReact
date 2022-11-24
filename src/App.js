c985b32c737b9bed879932c08d569810a2b2a942
Alien Rodríguez López <arl859@gmail.com>
2022-11-24 15:10:01 -0800

    FirstChange

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [linea1, setLinea1]= useState('');
+  const [linea2, setLinea2]= useState('');
+  const [imagen, setImagen]= useState('');
+
+  const onchangelinea1 = function(evento){
+    setLinea1(evento.target.value)
+  }
+
+  const onchangelinea2 = function(evento){
+    setLinea2(evento.target.value)
+  }
+  const onchangeImagen = function(evento){
+    setImagen(evento.target.value)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <select onChange={onchangeImagen}>
+        <option value="Alien">Alien</option>
+        <option value="Paulito">Mpaulito</option>
+        <option value="Liodine">Liodine</option>
+      </select> <br/>
+      <input onChange={onchangelinea1} type= "text" placeholder="Line 1"></input> <br/>
+      <input onChange={onchangelinea2} type= "text" placeholder="Line 2"></input> <br/>
+      <button>Add</button>
+      <div className='meme' >
+        <span>{linea1}</span><br/>
+        <img src= {'/img/'+ imagen + '.jpg'}></img>
+        <span>{linea2}</span>
+      </div>
+
     </div>
   );
 }
